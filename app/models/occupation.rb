97c15fdedcf13b991808ad20cfb0dd9c71d35e89
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-10-19 11:24:24 +0900

    モデルの関連付けをER図を参考に追記しました

--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -1,4 +1,6 @@
 class Occupation < ApplicationRecord
 	has_many :shops, through: :shop_occupations
-	has_many :shop_occupations
+	has_many :shop_occupations, dependent: :destroy
+	has_many :freelances, through: :freelance_occupations
+	has_many :freelance_occupations, dependent: :destroy
 end
