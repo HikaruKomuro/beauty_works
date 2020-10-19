c51b2c9718a87bd7052a1f422d578f430240a318
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-10-19 11:26:00 +0900

    すでに他のPRで作成済みファイルだが、テストを走らせるために作成した

--- spec/factories/freelances.rb
+++ spec/factories/freelances.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :freelance do
-    
+    sequence(:name) { |n| "sample#{n}" }
+    sequence(:email) { |n| "TEST#{n}@example.com" }
+    password            { "password" }
+    post_code           { "1234567" }
+    address_prefecture  { "Tokyo" }
+    address_city        { "tyuuou" }
+    address_branch      { "nihonbashi" }
   end
 end
