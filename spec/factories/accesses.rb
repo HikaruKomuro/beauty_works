80d825640d169824a66cd909228a885fefbb035b
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-10-24 12:03:07 +0900

    acessのモデルスペックテストを追加した

--- spec/factories/accesses.rb
+++ spec/factories/accesses.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :access do
-    
+    association :shop
   end
 end
--- spec/models/access_spec.rb
+++ spec/models/access_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Access, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:shop) { FactoryBot.create(:shop) }
+
+  # shop_idの存在
+  describe "presence of shop_id" do
+    # shop_idがあれば有効な状態であること
+    it "is valid with a shop_id" do
+      access = Access.new(shop_id: shop.id)
+      expect(access).to be_valid
+    end
+
+    # shop_idがなければ無効な状態であること
+    it "is invalid without a shop_id" do
+      access = Access.new(shop_id: nil)
+      access.valid?
+      expect(access.errors[:shop]).to include("を入力してください")
+    end
+  end
 end
