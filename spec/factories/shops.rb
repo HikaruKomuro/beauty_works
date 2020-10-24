1dba5f1a052ec30b26b08954a5c1db7c41cd0f2d
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-10-24 12:01:40 +0900

    accessのリレーション確認のために、ownerとshopモデルを作成されるようにした

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,5 +1,45 @@
 FactoryBot.define do
   factory :shop do
-    
+    association :owner
+    sequence(:name)       { |n| "sample#{n}" }
+    sequence(:email)      { |n| "TEST#{n}@example.com" }
+    sequence(:line_id )   { |n| "n" }
+    phone_number          { "08012345678" }
+    display               { "" }
+    note                  { "" }
+    post_code             { "1234567" }
+    address_prefecture    { "Tokyo" }
+    address_city          { "tyuuou" }
+    address_town          { "" }
+    address_branch        { "nihonbashi" }
+    address_building      { "building" }
+    mon_start             { "" }
+    mon_finish            { "" }
+    mon_off               { "" }
+    tue_start             { "" }
+    tue_finish            { "" }
+    tue_off               { "" }
+    wed_start             { "" }
+    wed_finish            { "" }
+    wed_off               { "" }
+    thu_start             { "" }
+    thu_finish            { "" }
+    thu_off               { "" }
+    fri_start             { "" }
+    fri_finish            { "" }
+    fri_off               { "" }
+    sat_start             { "" }
+    sat_finish            { "" }
+    sat_off               { "" }
+    sun_start             { "" }
+    sun_finish            { "" }
+    sun_off               { "" }
+    other_holiday         { "" }
+    appeal                { "" }
+    memo                  { "" }
+    introduction          { "" }
+    customer_sex          { "" }
+    worker_sex            { "" }
+    share                 { "" }
   end
 end
