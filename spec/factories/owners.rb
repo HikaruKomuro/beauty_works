2fd624170b9fc6fe822cdc52a20e000dea535113
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-10-26 08:32:12 +0900

    feeモデルテストのためにownerとshopモデルも作成

--- spec/factories/owners.rb
+++ spec/factories/owners.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :owner do
-    
+    sequence(:name) { |n| "sample#{n}" }
+    sequence(:email) { |n| "TEST#{n}@example.com" }
+    phone_number        { "08012345678" }
+    password            { "password" }
+    post_code           { "1234567" }
+    address_prefecture  { "Tokyo" }
+    address_city        { "tyuuou" }
+    address_branch      { "nihonbashi" }
   end
 end
